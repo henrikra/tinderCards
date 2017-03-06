3fab0bc6e6de60c203f051f04eeef7bdf109e471
Henrik Raitasola <henrik.raitasola@wolt.com>
2017-03-06 22:16:24 +0200

    Add basic dragging animation to card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,36 @@
 import React, {Component} from 'react';
-import {Image, Text, StyleSheet} from 'react-native';
+import {Image, Text, StyleSheet, PanResponder, Animated} from 'react-native';
 
 class Card extends Component {
+  state = {
+    position: new Animated.ValueXY(),
+  }
+
+  panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderGrant: () => {
+      this.state.position.setOffset({x: this.state.position.x._value, y: this.state.position.y._value});
+      this.state.position.setValue({x: 0, y: 0});
+    },
+    onPanResponderMove: Animated.event([null, {dx: this.state.position.x, dy: this.state.position.y}]),
+    onPanResponderRelease: () => {
+      this.state.position.flattenOffset();
+    },
+  })
+
   render() {
     const {name, age, image} = this.props;
     return (
-      <Image source={{uri: image}} style={styles.image}>
+      <Animated.Image 
+        source={{uri: image}} 
+        style={[
+          styles.image, 
+          {transform: this.state.position.getTranslateTransform()},
+        ]} 
+        {...this.panResponder.panHandlers}
+      >
         <Text style={styles.title}>{name}, {age}</Text>
-      </Image>
+      </Animated.Image>
     );
   }
 }
